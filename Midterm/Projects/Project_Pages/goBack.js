8eaf681b2ae49dfe607a592b036d19c94eb8c73e
S1H2A3-art <jq2385@nyu.edu>
2025-10-30 06:22:55 -0400

    goBack() added

---
+++ Midterm/Projects/Project_Pages/goBack.js
@@ -0,0 +1,10 @@
+function goBackOrHome() {
+  const referrer = document.referrer;
+  const currentOrigin = window.location.origin;
+
+  if (referrer && referrer.startsWith(currentOrigin)) {
+    history.back();
+  } else {
+    window.location.href = "../../../Navigation_Page/Explorer.html"; 
+  }
+}
